ab12579991a65700c1267894d8dde57e305d337b
Muriel Damazio <murieldps@gmail.com>
2021-11-08 09:20:05 -0300

    Feat: Home page done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import logoImg from '../../../public/images/logo.svg'
+import { SignInButton } from '../SignInButton'
 
 import styles from './styles.module.scss'
 
@@ -12,6 +13,7 @@ export function Header() {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+        <SignInButton/>
       </div>
     </header>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
+import avatarImg from '../../public/images/avatar.svg'
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import styles from './Home.module.scss'
 
 export default function Home() {
   return (
@@ -6,6 +12,19 @@ export default function Home() {
       <Head>
         <title>Home - ig.news</title>
         </Head>
+      
+      <main className={styles.mainWrapper}>
+        <section className={styles.sectionWrapper}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <strong>React</strong> world</h1>
+          <p className={styles.priceInfo}>Get acess to all the publications <br/>
+          <span>for $9,90 monthly</span>
+          </p>
+          <SubscribeButton/>
+        </section>
+
+        <Image src={avatarImg} alt="Girl coding" />
+      </main>
     </>
   )
 }
